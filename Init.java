9a9e7278c202162db46f5055f077cfbbea52d52e
nevMor <251070568@qq.com>
2017-06-16 10:59:58 +0800

    Update Init.java

--- Init.java
+++ Init.java
@@ -36,3 +36,193 @@ public Configuration() {
     languageRegistry.setDefaultDriverClass(XMLLanguageDriver.class);
     languageRegistry.register(RawLanguageDriver.class);
   }
+  
+可以看到初始化时注册了一堆别名，具体注册过程如下：
+
+public void registerAlias(Class<?> type) {
+    //如果没有类型别名，用Class.getSimpleName来注册
+    String alias = type.getSimpleName();
+	//或者通过Alias注解来注册(Class.getAnnotation)
+    Alias aliasAnnotation = type.getAnnotation(Alias.class);
+    if (aliasAnnotation != null) {
+      alias = aliasAnnotation.value();
+    } 
+    registerAlias(alias, type);
+  }
+
+ public void registerAlias(String alias, Class<?> value) {
+    if (alias == null) {
+      throw new TypeException("The parameter alias cannot be null");
+    }
+    // issue #748
+    String key = alias.toLowerCase(Locale.ENGLISH);
+    //如果已经存在key了，且value和之前不一致，报错
+    if (TYPE_ALIASES.containsKey(key) && TYPE_ALIASES.get(key) != null && !TYPE_ALIASES.get(key).equals(value)) {
+      throw new TypeException("The alias '" + alias + "' is already mapped to the value '" + TYPE_ALIASES.get(key).getName() + "'.");
+    }
+    TYPE_ALIASES.put(key, value);
+  }
+  
+ 进入org.apache.ibatis.type.TypeAliasRegistry 可以看到在构造函数中已经把jdk中的基础类和包装类注册进别名map中了
+ public class TypeAliasRegistry {
+
+  private final Map<String, Class<?>> TYPE_ALIASES = new HashMap<String, Class<?>>();
+
+  public TypeAliasRegistry() {
+		//构造函数里注册系统内置的类型别名
+    registerAlias("string", String.class);
+
+	//基本包装类型
+    registerAlias("byte", Byte.class);
+    registerAlias("long", Long.class);
+    registerAlias("short", Short.class);
+    registerAlias("int", Integer.class);
+    registerAlias("integer", Integer.class);
+    registerAlias("double", Double.class);
+    registerAlias("float", Float.class);
+    registerAlias("boolean", Boolean.class);
+
+	//基本数组包装类型
+    registerAlias("byte[]", Byte[].class);
+    registerAlias("long[]", Long[].class);
+    registerAlias("short[]", Short[].class);
+    registerAlias("int[]", Integer[].class);
+    registerAlias("integer[]", Integer[].class);
+    registerAlias("double[]", Double[].class);
+    registerAlias("float[]", Float[].class);
+    registerAlias("boolean[]", Boolean[].class);
+
+	//加个下划线，就变成了基本类型
+    registerAlias("_byte", byte.class);
+    registerAlias("_long", long.class);
+    registerAlias("_short", short.class);
+    registerAlias("_int", int.class);
+    registerAlias("_integer", int.class);
+    registerAlias("_double", double.class);
+    registerAlias("_float", float.class);
+    registerAlias("_boolean", boolean.class);
+
+	//加个下划线，就变成了基本数组类型
+    registerAlias("_byte[]", byte[].class);
+    registerAlias("_long[]", long[].class);
+    registerAlias("_short[]", short[].class);
+    registerAlias("_int[]", int[].class);
+    registerAlias("_integer[]", int[].class);
+    registerAlias("_double[]", double[].class);
+    registerAlias("_float[]", float[].class);
+    registerAlias("_boolean[]", boolean[].class);
+
+	//日期数字型
+    registerAlias("date", Date.class);
+    registerAlias("decimal", BigDecimal.class);
+    registerAlias("bigdecimal", BigDecimal.class);
+    registerAlias("biginteger", BigInteger.class);
+    registerAlias("object", Object.class);
+
+    registerAlias("date[]", Date[].class);
+    registerAlias("decimal[]", BigDecimal[].class);
+    registerAlias("bigdecimal[]", BigDecimal[].class);
+    registerAlias("biginteger[]", BigInteger[].class);
+    registerAlias("object[]", Object[].class);
+
+	//集合型
+    registerAlias("map", Map.class);
+    registerAlias("hashmap", HashMap.class);
+    registerAlias("list", List.class);
+    registerAlias("arraylist", ArrayList.class);
+    registerAlias("collection", Collection.class);
+    registerAlias("iterator", Iterator.class);
+
+	//还有个ResultSet型
+    registerAlias("ResultSet", ResultSet.class);
+  }
+  
+  另外初始化 TypeHandlerRegistry.TypeHandlerRegistry类
+  此类将jdbcType转为java类，如下所示：
+   public TypeHandlerRegistry() {
+    //构造函数里注册系统内置的类型处理器
+	  //以下是为多个类型注册到同一个handler
+    register(Boolean.class, new BooleanTypeHandler());
+    register(boolean.class, new BooleanTypeHandler());
+    register(JdbcType.BOOLEAN, new BooleanTypeHandler());
+    register(JdbcType.BIT, new BooleanTypeHandler());
+
+    register(Byte.class, new ByteTypeHandler());
+    register(byte.class, new ByteTypeHandler());
+    register(JdbcType.TINYINT, new ByteTypeHandler());
+
+    register(Short.class, new ShortTypeHandler());
+    register(short.class, new ShortTypeHandler());
+    register(JdbcType.SMALLINT, new ShortTypeHandler());
+
+    register(Integer.class, new IntegerTypeHandler());
+    register(int.class, new IntegerTypeHandler());
+    register(JdbcType.INTEGER, new IntegerTypeHandler());
+
+    register(Long.class, new LongTypeHandler());
+    register(long.class, new LongTypeHandler());
+
+    register(Float.class, new FloatTypeHandler());
+    register(float.class, new FloatTypeHandler());
+    register(JdbcType.FLOAT, new FloatTypeHandler());
+
+    register(Double.class, new DoubleTypeHandler());
+    register(double.class, new DoubleTypeHandler());
+    register(JdbcType.DOUBLE, new DoubleTypeHandler());
+
+	  //以下是为同一个类型的多种变种注册到多个不同的handler
+    register(String.class, new StringTypeHandler());
+    register(String.class, JdbcType.CHAR, new StringTypeHandler());
+    register(String.class, JdbcType.CLOB, new ClobTypeHandler());
+    register(String.class, JdbcType.VARCHAR, new StringTypeHandler());
+    register(String.class, JdbcType.LONGVARCHAR, new ClobTypeHandler());
+    register(String.class, JdbcType.NVARCHAR, new NStringTypeHandler());
+    register(String.class, JdbcType.NCHAR, new NStringTypeHandler());
+    register(String.class, JdbcType.NCLOB, new NClobTypeHandler());
+    register(JdbcType.CHAR, new StringTypeHandler());
+    register(JdbcType.VARCHAR, new StringTypeHandler());
+    register(JdbcType.CLOB, new ClobTypeHandler());
+    register(JdbcType.LONGVARCHAR, new ClobTypeHandler());
+    register(JdbcType.NVARCHAR, new NStringTypeHandler());
+    register(JdbcType.NCHAR, new NStringTypeHandler());
+    register(JdbcType.NCLOB, new NClobTypeHandler());
+
+    register(Object.class, JdbcType.ARRAY, new ArrayTypeHandler());
+    register(JdbcType.ARRAY, new ArrayTypeHandler());
+
+    register(BigInteger.class, new BigIntegerTypeHandler());
+    register(JdbcType.BIGINT, new LongTypeHandler());
+
+    register(BigDecimal.class, new BigDecimalTypeHandler());
+    register(JdbcType.REAL, new BigDecimalTypeHandler());
+    register(JdbcType.DECIMAL, new BigDecimalTypeHandler());
+    register(JdbcType.NUMERIC, new BigDecimalTypeHandler());
+
+    register(Byte[].class, new ByteObjectArrayTypeHandler());
+    register(Byte[].class, JdbcType.BLOB, new BlobByteObjectArrayTypeHandler());
+    register(Byte[].class, JdbcType.LONGVARBINARY, new BlobByteObjectArrayTypeHandler());
+    register(byte[].class, new ByteArrayTypeHandler());
+    register(byte[].class, JdbcType.BLOB, new BlobTypeHandler());
+    register(byte[].class, JdbcType.LONGVARBINARY, new BlobTypeHandler());
+    register(JdbcType.LONGVARBINARY, new BlobTypeHandler());
+    register(JdbcType.BLOB, new BlobTypeHandler());
+
+    register(Object.class, UNKNOWN_TYPE_HANDLER);
+    register(Object.class, JdbcType.OTHER, UNKNOWN_TYPE_HANDLER);
+    register(JdbcType.OTHER, UNKNOWN_TYPE_HANDLER);
+
+    register(Date.class, new DateTypeHandler());
+    register(Date.class, JdbcType.DATE, new DateOnlyTypeHandler());
+    register(Date.class, JdbcType.TIME, new TimeOnlyTypeHandler());
+    register(JdbcType.TIMESTAMP, new DateTypeHandler());
+    register(JdbcType.DATE, new DateOnlyTypeHandler());
+    register(JdbcType.TIME, new TimeOnlyTypeHandler());
+
+    register(java.sql.Date.class, new SqlDateTypeHandler());
+    register(java.sql.Time.class, new SqlTimeTypeHandler());
+    register(java.sql.Timestamp.class, new SqlTimestampTypeHandler());
+
+    // issue #273
+    register(Character.class, new CharacterTypeHandler());
+    register(char.class, new CharacterTypeHandler());
+  }
