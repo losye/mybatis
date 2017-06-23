09bbb063045448a9a0105d8ccf44d0360bbc68e2
nevMor <251070568@qq.com>
2017-06-23 15:01:13 +0800

    Update Plugin.java

--- Plugin.java
+++ Plugin.java
@@ -167,3 +167,137 @@ public class Plugin implements InvocationHandler {
   }
 
 }
+
+通常我们使用插件PageHelper.startPage方法
+@SuppressWarnings({"rawtypes", "unchecked"})
+@Intercepts(
+    {
+        @Signature(type = Executor.class, method = "query", args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class}),
+        @Signature(type = Executor.class, method = "query", args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class, CacheKey.class, BoundSql.class}),
+    }
+)
+public class PageInterceptor implements Interceptor {
+    //缓存count查询的ms
+    protected Cache<CacheKey, MappedStatement> msCountMap = null;
+    private Dialect dialect;
+    private String default_dialect_class = "com.github.pagehelper.PageHelper";
+    private Field additionalParametersField;
+
+    @Override
+    public Object intercept(Invocation invocation) throws Throwable {
+        try {
+            Object[] args = invocation.getArgs();
+            MappedStatement ms = (MappedStatement) args[0];
+            Object parameter = args[1];
+            RowBounds rowBounds = (RowBounds) args[2];
+            ResultHandler resultHandler = (ResultHandler) args[3];
+            Executor executor = (Executor) invocation.getTarget();
+            CacheKey cacheKey;
+            BoundSql boundSql;
+            //由于逻辑关系，只会进入一次
+            if(args.length == 4){
+                //4 个参数时
+                boundSql = ms.getBoundSql(parameter);
+                cacheKey = executor.createCacheKey(ms, parameter, rowBounds, boundSql);
+            } else {
+                //6 个参数时
+                cacheKey = (CacheKey) args[4];
+                boundSql = (BoundSql) args[5];
+            }
+            List resultList;
+            //调用方法判断是否需要进行分页，如果不需要，直接返回结果
+            if (!dialect.skip(ms, parameter, rowBounds)) {
+                //反射获取动态参数
+                Map<String, Object> additionalParameters = (Map<String, Object>) additionalParametersField.get(boundSql);
+                //判断是否需要进行 count 查询
+                if (dialect.beforeCount(ms, parameter, rowBounds)) {
+                    //创建 count 查询的缓存 key
+                    CacheKey countKey = executor.createCacheKey(ms, parameter, RowBounds.DEFAULT, boundSql);
+                    countKey.update(MSUtils.COUNT);
+                    MappedStatement countMs = msCountMap.get(countKey);
+                    if (countMs == null) {
+                        //根据当前的 ms 创建一个返回值为 Long 类型的 ms
+                        countMs = MSUtils.newCountMappedStatement(ms);
+                        msCountMap.put(countKey, countMs);
+                    }
+                    //调用方言获取 count sql
+                    String countSql = dialect.getCountSql(ms, boundSql, parameter, rowBounds, countKey);
+                    countKey.update(countSql);
+                    BoundSql countBoundSql = new BoundSql(ms.getConfiguration(), countSql, boundSql.getParameterMappings(), parameter);
+                    //当使用动态 SQL 时，可能会产生临时的参数，这些参数需要手动设置到新的 BoundSql 中
+                    for (String key : additionalParameters.keySet()) {
+                        countBoundSql.setAdditionalParameter(key, additionalParameters.get(key));
+                    }
+                    //执行 count 查询
+                    Object countResultList = executor.query(countMs, parameter, RowBounds.DEFAULT, resultHandler, countKey, countBoundSql);
+                    Long count = (Long) ((List) countResultList).get(0);
+                    //处理查询总数
+                    //返回 true 时继续分页查询，false 时直接返回
+                    if (!dialect.afterCount(count, parameter, rowBounds)) {
+                        //当查询总数为 0 时，直接返回空的结果
+                        return dialect.afterPage(new ArrayList(), parameter, rowBounds);
+                    }
+                }
+                //判断是否需要进行分页查询
+                if (dialect.beforePage(ms, parameter, rowBounds)) {
+                    //生成分页的缓存 key
+                    CacheKey pageKey = cacheKey;
+                    //处理参数对象
+                    parameter = dialect.processParameterObject(ms, parameter, boundSql, pageKey);
+                    //调用方言获取分页 sql
+                    String pageSql = dialect.getPageSql(ms, boundSql, parameter, rowBounds, pageKey);
+                    BoundSql pageBoundSql = new BoundSql(ms.getConfiguration(), pageSql, boundSql.getParameterMappings(), parameter);
+                    //设置动态参数
+                    for (String key : additionalParameters.keySet()) {
+                        pageBoundSql.setAdditionalParameter(key, additionalParameters.get(key));
+                    }
+                    //执行分页查询
+                    resultList = executor.query(ms, parameter, RowBounds.DEFAULT, resultHandler, pageKey, pageBoundSql);
+                } else {
+                    //不执行分页的情况下，也不执行内存分页
+                    resultList = executor.query(ms, parameter, RowBounds.DEFAULT, resultHandler, cacheKey, boundSql);
+                }
+            } else {
+                //rowBounds用参数值，不使用分页插件处理时，仍然支持默认的内存分页
+                resultList = executor.query(ms, parameter, rowBounds, resultHandler, cacheKey, boundSql);
+            }
+            return dialect.afterPage(resultList, parameter, rowBounds);
+        } finally {
+            dialect.afterAll();
+        }
+    }
+
+    @Override
+    public Object plugin(Object target) {
+        //TODO Spring bean 方式配置时，如果没有配置属性就不会执行下面的 setProperties 方法，就不会初始化，因此考虑在这个方法中做一次判断和初始化
+        //TODO https://github.com/pagehelper/Mybatis-PageHelper/issues/26
+        return Plugin.wrap(target, this);
+    }
+
+    @Override
+    public void setProperties(Properties properties) {
+        //缓存 count ms
+        msCountMap = CacheFactory.createCache(properties.getProperty("msCountCache"), "ms", properties);
+        String dialectClass = properties.getProperty("dialect");
+        if (StringUtil.isEmpty(dialectClass)) {
+            dialectClass = default_dialect_class;
+        }
+        try {
+            Class<?> aClass = Class.forName(dialectClass);
+            dialect = (Dialect) aClass.newInstance();
+        } catch (Exception e) {
+            throw new PageException(e);
+        }
+        dialect.setProperties(properties);
+        try {
+            //反射获取 BoundSql 中的 additionalParameters 属性
+            additionalParametersField = BoundSql.class.getDeclaredField("additionalParameters");
+            additionalParametersField.setAccessible(true);
+        } catch (NoSuchFieldException e) {
+            throw new PageException(e);
+        }
+    }
+
+}
+
+
