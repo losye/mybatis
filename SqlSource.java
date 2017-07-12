0597cbf768a3782e3fda0477b6870e3a9e33c4ef
nevMor <251070568@qq.com>
2017-07-12 20:06:32 +0800

    Update SqlSource.java

--- SqlSource.java
+++ SqlSource.java
@@ -69,3 +69,89 @@ public class DefaultParameterHandler implements ParameterHandler {
   }
 
 }
+
+public class SqlSourceBuilder extends BaseBuilder {
+  
+   public SqlSource parse(String originalSql, Class<?> parameterType, Map<String, Object> additionalParameters) {
+    ParameterMappingTokenHandler handler = new ParameterMappingTokenHandler(configuration, parameterType, additionalParameters);
+    //替换#{}中间的部分,如何替换，逻辑在ParameterMappingTokenHandler
+    GenericTokenParser parser = new GenericTokenParser("#{", "}", handler);
+    String sql = parser.parse(originalSql);
+    //返回静态SQL源码
+    return new StaticSqlSource(configuration, sql, handler.getParameterMappings());
+  }
+  
+  private static class ParameterMappingTokenHandler extends BaseBuilder implements TokenHandler {
+      @Override
+    public String handleToken(String content) {
+      //先构建参数映射
+      parameterMappings.add(buildParameterMapping(content));
+      //如何替换很简单，永远是一个问号，但是参数的信息要记录在parameterMappings里面供后续使用
+      return "?";
+    }
+    private ParameterMapping buildParameterMapping(String content) {
+        //#{favouriteSection,jdbcType=VARCHAR}
+        //先解析参数映射,就是转化成一个hashmap
+      Map<String, String> propertiesMap = parseParameterMapping(content);
+      String property = propertiesMap.get("property");
+      Class<?> propertyType;
+      //这里分支比较多，需要逐个理解
+      if (metaParameters.hasGetter(property)) { // issue #448 get type from additional params
+        propertyType = metaParameters.getGetterType(property);
+      } else if (typeHandlerRegistry.hasTypeHandler(parameterType)) {
+        propertyType = parameterType;
+      } else if (JdbcType.CURSOR.name().equals(propertiesMap.get("jdbcType"))) {
+        propertyType = java.sql.ResultSet.class;
+      } else if (property != null) {
+        MetaClass metaClass = MetaClass.forClass(parameterType);
+        if (metaClass.hasGetter(property)) {
+          propertyType = metaClass.getGetterType(property);
+        } else {
+          propertyType = Object.class;
+        }
+      } else {
+        propertyType = Object.class;
+      }
+      ParameterMapping.Builder builder = new ParameterMapping.Builder(configuration, property, propertyType);
+      Class<?> javaType = propertyType;
+      String typeHandlerAlias = null;
+      for (Map.Entry<String, String> entry : propertiesMap.entrySet()) {
+        String name = entry.getKey();
+        String value = entry.getValue();
+        if ("javaType".equals(name)) {
+          javaType = resolveClass(value);
+          builder.javaType(javaType);
+        } else if ("jdbcType".equals(name)) {
+          builder.jdbcType(resolveJdbcType(value));
+        } else if ("mode".equals(name)) {
+          builder.mode(resolveParameterMode(value));
+        } else if ("numericScale".equals(name)) {
+          builder.numericScale(Integer.valueOf(value));
+        } else if ("resultMap".equals(name)) {
+          builder.resultMapId(value);
+        } else if ("typeHandler".equals(name)) {
+          typeHandlerAlias = value;
+        } else if ("jdbcTypeName".equals(name)) {
+          builder.jdbcTypeName(value);
+        } else if ("property".equals(name)) {
+          // Do Nothing
+        } else if ("expression".equals(name)) {
+          throw new BuilderException("Expression based parameters are not supported yet");
+        } else {
+          throw new BuilderException("An invalid property '" + name + "' was found in mapping #{" + content + "}.  Valid properties are " + parameterProperties);
+        }
+      }
+      //#{age,javaType=int,jdbcType=NUMERIC,typeHandler=MyTypeHandler}
+      if (typeHandlerAlias != null) {
+        builder.typeHandler(resolveTypeHandler(javaType, typeHandlerAlias));
+      }
+      return builder.build();
+    }
+    
+  }
+
+}
+
+
+
+
