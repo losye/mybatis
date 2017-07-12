b6cdc946f0c495d59f9f8ed483317c8444107c3d
nevMor <251070568@qq.com>
2017-07-12 19:46:53 +0800

    Update JdbcType2JavaType.java

--- JdbcType2JavaType.java
+++ JdbcType2JavaType.java
@@ -1,1 +1,48 @@
 JdbcType2JavaType 在mybatis中的转换
+
+public interface TypeHandler<T> {
+
+  //设置参数
+  void setParameter(PreparedStatement ps, int i, T parameter, JdbcType jdbcType) throws SQLException;
+
+  //取得结果,供普通select用
+  T getResult(ResultSet rs, String columnName) throws SQLException;
+
+  //取得结果,供普通select用
+  T getResult(ResultSet rs, int columnIndex) throws SQLException;
+
+  //取得结果,供SP用
+  T getResult(CallableStatement cs, int columnIndex) throws SQLException;
+
+}
+
+
+
+public class StringTypeHandler extends BaseTypeHandler<String> {
+
+  @Override
+  public void setNonNullParameter(PreparedStatement ps, int i, String parameter, JdbcType jdbcType)
+      throws SQLException {
+    ps.setString(i, parameter);
+  }
+
+  @Override
+  public String getNullableResult(ResultSet rs, String columnName)
+      throws SQLException {
+    return rs.getString(columnName);
+  }
+
+  @Override
+  public String getNullableResult(ResultSet rs, int columnIndex)
+      throws SQLException {
+    return rs.getString(columnIndex);
+  }
+
+  @Override
+  public String getNullableResult(CallableStatement cs, int columnIndex)
+      throws SQLException {
+    return cs.getString(columnIndex);
+  }
+}
+
+
